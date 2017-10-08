96a213a2af54c1108252b50051fa1c8ab2f178cd
rogertecnic <roger_tecnic@hotmail.com>
2017-10-08 01:37:44 -0300

    commit de alteracao local, antes do commit online

--- projeto/sor_file.cpp
+++ projeto/sor_file.cpp
@@ -1,11 +1,14 @@
+// adicionei localmente
+
 /*
  * sor_file.cpp
  *
  * alterei localmente no meio
  *
- * alteracao online acima do Created on
  *  Created on: 8 de out de 2017
  *      Author: rogerio
  *
  *      criei este arquivo pelo eclipse
  */
+
+// adicionei localmente
