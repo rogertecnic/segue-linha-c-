b4fa8ad84d30a1dae6260731ef63a8336909ecdf
rogertecnic <roger_tecnic@hotmail.com>
2017-10-08 01:39:49 -0300

    alteracao local antes da online

--- projeto/sor_file.cpp
+++ projeto/sor_file.cpp
@@ -1,11 +1,11 @@
-/*
- * sor_file.cpp
- *
- * alterei localmente no meio
- *
- *  Created on: 8 de out de 2017
- *      Author: rogerio
- *
- *      criei este arquivo pelo eclipse
- */
+
+// * sor_file.cpp
+// *
+// * alterei localmente no meio
+// *
+// *  Created on: 8 de out de 2017
+// *      Author: rogerio
+// *
+// *      criei este arquivo pelo eclipse
+
 
