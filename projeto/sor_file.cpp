c31957c304fd128c07baffd9beb1d8c864baf84c
Rogério Pereira Batista <roger_tecnic@hotmail.com>
2017-10-08 01:22:24 -0300

    apaguei denovo online

--- projeto/sor_file.cpp
+++ projeto/sor_file.cpp
@@ -6,13 +6,3 @@
  *
  *      criei este arquivo pelo eclipse
  */
-
-/*
- * alteracao local em uma posicao do documento diferente
- */
-
-
-
-/*
- * alterei local depois de ter apagado tudo online, supostamente em locais diferentes
- */
