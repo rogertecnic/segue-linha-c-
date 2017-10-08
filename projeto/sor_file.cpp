18bc554e01142d94cbca0d193e74121bd1fbcf49
rogertecnic <roger_tecnic@hotmail.com>
2017-10-08 11:06:27 -0300

    limpei o source fole

--- projeto/sor_file.cpp
+++ projeto/sor_file.cpp
@@ -1,17 +1,6 @@
-
-// * sor_file.cpp
-// *
-// * alterei localmente no meio
-// *
-// *  Created on: 8 de out de 2017
-// *      Author: rogerio
-// *
-// criei essa linha online
-// *      criei este arquivo pelo eclipse
-
-
-
-
-// essa linha online eu criei
-
-// alteracao online 
+//
+//
+//
+//
+//
+//
