c35e6eff1778950c2e9584c955317486546dd4ac
Rogério Pereira Batista <roger_tecnic@hotmail.com>
2017-10-08 01:38:12 -0300

    commit online depois do local

--- projeto/sor_file.cpp
+++ projeto/sor_file.cpp
@@ -6,6 +6,6 @@
  * alteracao online acima do Created on
  *  Created on: 8 de out de 2017
  *      Author: rogerio
- *
+ * alteracao depois do Created online
  *      criei este arquivo pelo eclipse
  */
